5833041de976d14b9129716157bfe4abbc131296
Bas van Ritbergen <1212915+Ritbit@users.noreply.github.com>
2024-10-21 12:53:51 +0200

    Added GPL3 license

--- src/sight.ino
+++ src/sight.ino
@@ -1,17 +1,31 @@
 /*
-Name    : SIGHT (Shelf Indicators for Guided Handling Tasks)
-Version : 1.6
-Date    : 2024-10-01
-Author  : Bas van Ritbergen <bas.vanritbergen@adyen.com> / bas@ritbit.com
-
-Note    :
-
-Version Descrption :
-  * Replaced patterns with animations.
+Name        : SIGHT (Shelf Indicators for Guided Handling Tasks)
+Version     : 1.6
+Date        : 2024-10-01
+Author      : Bas van Ritbergen <bas.vanritbergen@adyen.com> / bas@ritbit.com
+Description : Replaced patterns with animations.
+Notes       : When compiling make sure to reserve a little space for littleFS (8-64k)
 
 TODO:
       The RGB/RGBW switch does not work properly yet since I made the RGB/RGBW option selectable in the config.
       Need to find a fix for this, in the mean time only RGB leds are supported !	
+
+
+Copyright (C) 2024  Bas van Ritbergen
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 */
 
 // Basic defaults 
